3b6f6a51293098a1b220fb70773631e9ba3322e2
RYAN <RTIPPER44@GMAIL.COM>
2021-04-22 09:42:45 -0700

    lab 18 first push

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const friendModel = require('./schema.js');
+
+const uuid = require('uuid').v4;
+
+exports.handler = async (event) => {
+
+  console.log(event);
+
+  try {
+
+    const body = JSON.parse(event.body);
+    const { name, phone } = body;
+    const id = uuid();
+
+    const record = new friendModel({ id, name, phone });
+    const data = await record.save();
+    const savedRecord = JSON.stringify(data);
+
+
+    return {
+      statusCode: 201,
+      response: savedRecord
+    }
+
+  } catch (e) {
+    return {
+      statusCode: 500,
+      response: e.message
+    }
+
+  }
+
+};
